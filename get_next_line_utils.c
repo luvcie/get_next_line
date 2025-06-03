89604fd99ca83a27bedf130652d8c0a425a5baef
luvcie <lucpardo@student.42.fr>
2025-06-03 20:16:55 +0200

    add ft_strchr

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 12:51:37 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/06/03 20:13:50 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/06/03 20:16:41 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -21,4 +21,24 @@ while (str[i] != '\0')
 		i++;
 	}
 	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == (unsigned char)c)
+		{
+			return ((char *)str + i);
+		}
+		++i;
+	}
+	if (str[i] == '\0' && (unsigned char)c == '\0')
+	{
+		return ((char *)str + i);
+	}
+	return (NULL);
 }
