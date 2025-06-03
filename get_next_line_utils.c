0ba8b836d86a49acb1a86bc479d111310f07f1a6
luvcie <lucpardo@student.42.fr>
2025-06-03 20:35:49 +0200

    add ft_substr adapted for gnl

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 12:51:37 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/06/03 20:16:41 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/06/03 20:35:11 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -16,7 +16,7 @@ size_t	ft_strlen(const char *str)
 	size_t	i;
 
 	i = 0;
-while (str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		i++;
 	}
@@ -41,4 +41,33 @@ char	*ft_strchr(const char *str, int c)
 		return ((char *)str + i);
 	}
 	return (NULL);
+}
+
+char	*ft_gnl_substr(char const *str, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	slen;
+	size_t	i;
+
+	if (str == NULL)
+		return (NULL);
+	slen = ft_strlen(str);
+	if (start >= slen)
+	{
+		ptr = malloc(1);
+		if (ptr)
+			ptr[0] = '\0';
+		return (ptr);
+	}
+	if (len > slen - start)
+		len = slen - start;
+	ptr = malloc(len + 1);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+		ptr[i] = str[start + i];
+		i++;
+	ptr[i] = '\0';
+	return (ptr);
 }
